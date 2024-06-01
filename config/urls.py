80c18886e7941aecebd6327b461ef72485b35c97
ABROR-build <142251145+ABROR-build@users.noreply.github.com>
2024-06-01 18:42:54 +0500

    registration and login view templates added

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, docment_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('users/', include('users.urls'))
+] + static(settings.MEDIA_URL, docment_root=settings.MEDIA_ROOT)
 
