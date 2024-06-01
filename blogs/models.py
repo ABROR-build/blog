9dba9d4d66263e18abdb576a5422b9547a44853d
Arabboydev <157864498+Arabboydev@users.noreply.github.com>
2024-06-01 17:22:24 +0500

    blogs model written

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/',blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'post'
+
+    def __str__(self):
+        return self.title
+
+
